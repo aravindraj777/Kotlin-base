95b44ccc88bdc114ea164fd0737cb74a4a7b9aec
Aravind <aravindmunna240@gmail.com>
2024-11-24 12:28:27 +0530

    first commit

---
+++ src/test/kotlin/com/buildlive/kotlin_base/KotlinBaseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.buildlive.kotlin_base
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBaseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
