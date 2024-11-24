056efc44f845733d3bff9039a9ec571c0fb20abb
Aravind <aravindmunna240@gmail.com>
2024-11-24 21:29:12 +0530

    Restcontrollers - Mutable list returning

--- src/main/kotlin/com/buildlive/kotlin_base/RestControllers.kt
+++ src/main/kotlin/com/buildlive/kotlin_base/RestControllers.kt
@@ -1,2 +1,16 @@
 package com.buildlive.kotlin_base
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/articles")
+class ArticleController {
+
+    val articles = mutableListOf(Article(title = "My title", content = "My content"))
+
+    @GetMapping
+    fun articles() = articles
+
+}
