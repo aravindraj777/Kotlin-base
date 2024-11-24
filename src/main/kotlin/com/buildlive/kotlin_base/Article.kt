a7c88a6f2889d702757ae46359b4ba4d37411a30
Aravind <aravindmunna240@gmail.com>
2024-11-24 21:28:46 +0530

    :Data class in kotlin - records in java

--- src/main/kotlin/com/buildlive/kotlin_base/Article.kt
+++ src/main/kotlin/com/buildlive/kotlin_base/Article.kt
@@ -1,3 +1,12 @@
 package com.buildlive.kotlin_base
 
-data class Article()
+import java.time.LocalDateTime
+
+data class Article(
+
+        val  title: String,
+        val  content: String,
+        val createdAt: LocalDateTime = LocalDateTime.now(),
+        val slug:String = title.toSlug()
+
+)
