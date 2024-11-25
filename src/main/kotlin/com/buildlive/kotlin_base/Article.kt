71a0fd5d10b8bbb500478cb0353ab1074932cf1f
Aravind <aravindmunna240@gmail.com>
2024-11-25 16:45:25 +0530

    changed the val variables to var , its not const/final

--- src/main/kotlin/com/buildlive/kotlin_base/Article.kt
+++ src/main/kotlin/com/buildlive/kotlin_base/Article.kt
@@ -4,9 +4,9 @@ import java.time.LocalDateTime
 
 data class Article(
 
-        val  title: String,
-        val  content: String,
-        val createdAt: LocalDateTime = LocalDateTime.now(),
-        val slug:String = title.toSlug()
+        var title: String,
+        var content: String,
+        var createdAt: LocalDateTime = LocalDateTime.now(),
+        var slug:String = title.toSlug()
 
 )
