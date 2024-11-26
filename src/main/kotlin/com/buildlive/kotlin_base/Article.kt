d4c176cbeaad13244955388d4c2de3816c90f853
Aravind <aravindmunna240@gmail.com>
2024-11-26 16:20:27 +0530

    Data class changed to entity file

--- src/main/kotlin/com/buildlive/kotlin_base/Article.kt
+++ src/main/kotlin/com/buildlive/kotlin_base/Article.kt
@@ -2,11 +2,11 @@ package com.buildlive.kotlin_base
 
 import java.time.LocalDateTime
 
-data class Article(
-
-        var title: String,
-        var content: String,
-        var createdAt: LocalDateTime = LocalDateTime.now(),
-        var slug:String = title.toSlug()
-
-)
+//data class Article(
+//
+//        var title: String,
+//        var content: String,
+//        var createdAt: LocalDateTime = LocalDateTime.now(),
+//        var slug:String = title.toSlug()
+//
+//)
--- src/main/kotlin/com/buildlive/kotlin_base/Entities.kt
+++ src/main/kotlin/com/buildlive/kotlin_base/Entities.kt
@@ -1,2 +1,21 @@
 package com.buildlive.kotlin_base
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import org.springframework.boot.autoconfigure.web.WebProperties.Resources.Chain.Strategy
+import java.time.LocalDateTime
+
+@Entity
+ class Article(
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY )
+        var id: Long? = null,
+        var title: String,
+        var content: String,
+        var createdAt: LocalDateTime = LocalDateTime.now(),
+        var slug:String = title.toSlug()
+
+)
