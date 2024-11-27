110ad1614e8460919a12c9425f96919279b5e28e
Aravind <aravindmunna240@gmail.com>
2024-11-27 18:14:18 +0530

    Correction : Repo

--- src/main/kotlin/com/buildlive/kotlin_base/Repositories.kt
+++ src/main/kotlin/com/buildlive/kotlin_base/Repositories.kt
@@ -1,8 +1,10 @@
 package com.buildlive.kotlin_base
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import java.util.Optional
 
+@Repository
 interface IArticleRepository : JpaRepository<Article, Long> {
 
 
